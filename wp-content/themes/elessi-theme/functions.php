25b3b612996f5e1281edd1776c25073a564358d3
root <root@ip-172-31-11-153.ca-central-1.compute.internal>
2019-07-24 02:10:13 +0000

    removed pay now button from product page

--- wp-content/themes/elessi-theme/functions.php
+++ wp-content/themes/elessi-theme/functions.php
@@ -20,6 +20,7 @@ if (!isset($content_width)){
  */
 require_once ELESSI_THEME_PATH . '/options/nasa-options.php';
 
+add_filter( 'wc_stripe_hide_payment_request_on_product_page', '__return_true' );
 add_action('after_setup_theme', 'elessi_setup');
 if (!function_exists('elessi_setup')) :
 
