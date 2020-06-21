d8bb25eb017d8d13191aed7178d406e130d9a117
Irfan <irfan@merchkart.com>
2020-06-21 22:06:00 +0000

    Updated version P2L

--- wp-content/w3tc-config/master.php
+++ wp-content/w3tc-config/master.php
@@ -1,5 +1,5 @@
 <?php exit; ?>{
-    "version": "0.14.1",
+    "version": "0.14.2",
     "cluster.messagebus.debug": false,
     "cluster.messagebus.enabled": false,
     "cluster.messagebus.sns.region": "",
